a1d2c33b9104c6578fd34688debad75deb5e6093
saengle <zohv817@gmail.com>
2024-05-22 17:51:00 +0900

    [Feat] 기록 삭제 버튼 추가

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var countList = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     var emotionList = ["행복해", "사랑해", "좋아해", "화났다", "보통얌", "잠온다", "식은땀", "속상해", "맘아파"]
     
@@ -21,23 +21,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         titleLabel.text = "감정 다이어리"
-        checkMyData()
+        
         for i in 0...emotionLabelList.count - 1 {
             emotionLabelList[i].tag = i
-            emotionLabelList[i].text = emotionList[i]
             emotionButtonList[i].tag = i
-            emotionLabelList[i].text = "\(emotionList[i]) \(countList[i])"
         }
-        
+        checkMyData()
     }
     private func checkMyData() {
         guard let oldData = UserDefaults.standard.array(forKey: "data") else {
-            print("읽어오는 과정에서 문제가 있습니다.")
+            print("읽어오는 과정에 문제가 있습니다.")
+            setCountNumUI()
             return
         }
         countList = oldData as! [Int]
-        
-        print(#function, countList)
+        setCountNumUI()
+    }
+    
+    private func setCountNumUI() {
+        for i in 0...emotionList.count - 1 {
+            if countList[i] == 0 {
+                emotionLabelList[i].text = "\(emotionList[i])"
+            } else {
+                emotionLabelList[i].text = "\(emotionList[i]) \(countList[i])"
+            }
+        }
     }
     
     @IBAction func ImageButtonTapped(_ sender: UIButton) {
@@ -45,10 +53,13 @@ class ViewController: UIViewController {
         countList[tag] += 1
         emotionLabelList[tag].text = "\(emotionList[tag]) \(countList[tag])"
         UserDefaults.standard.set(countList, forKey: "data")
-        
-        
     }
     
+    @IBAction func deleteButtonClicked(_ sender: UIButton) {
+        UserDefaults.standard.removeObject(forKey: "data")
+        setCountNumUI()
+        countList = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
     
 }
 
